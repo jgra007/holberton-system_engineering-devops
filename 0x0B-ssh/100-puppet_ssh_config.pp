6a8ac0bc9c56893352415e2cec9a317a71e3ed20
= <=>
2021-04-05 18:01:47 +0000

    x

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Create the ~/.ssh/config with puppet
 file {'~/.ssh/config':
-  ensure  => present,
-  replace => 'yes',
-  path    => '~/.ssh/config',
-  content => 'Host *
-     HostName 35.196.227.93
-     User root
-     IdentityFile ~/.ssh/holberton',
-  mode    => '7000',
+ensure  => present,
+replace => 'yes',
+path    => '~/.ssh/config',
+content => 'Host *
+  HostName 35.196.227.93
+  User root
+  IdentityFile ~/.ssh/holberton',
+mode    => '7000',
 }
