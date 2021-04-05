861abb0b600e455cc709631294a67720e31544e4
= <=>
2021-04-05 17:22:31 +0000

    x!

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,7 +4,7 @@ file {'~/.ssh/config':
   replace => 'yes',
   path    => '~/.ssh/config',
   content => 'Host *
-     HostName 34.73.207.1
+     HostName 35.196.227.93
      User root
      IdentityFile ~/.ssh/holberton',
   mode    => '7000',
